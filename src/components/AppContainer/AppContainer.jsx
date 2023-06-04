d803a69df2f469ee05c8794cbaba94ae6324f577
YauheniKorsakas <yauheni.korsakas@gmail.com>
2023-06-04 14:02:59 +0300

    Refactor template. Add contacts

--- src/components/AppContainer/AppContainer.jsx
+++ src/components/AppContainer/AppContainer.jsx
@@ -1,9 +1,22 @@
-import Main from '../Main/Main';
+import Details from '../Details/Details';
 import styles from './AppContainer.module.scss';
+import Container from '../shared/Container/Container';
+import Contacts from '../Contacts/Contacts';
 
 const AppContainer = () => {
   return (<div className={styles.AppContainer}>
-    <Main />
+    <Container className={styles.CenterContainer}>
+      <div className={styles.TopContainer}>
+        <Details className={styles.Details}/>
+        {/* <div className={styles.Details}>Details</div> */}
+        <div className={styles.About}>About</div>
+        <Contacts className={styles.Contacts} />
+      </div>
+      <div className={styles.BottomContainer}>
+        <div className={styles.Skills}>Skills</div>
+        <div className={styles.Experience}>Experience</div>
+      </div>
+    </Container>
   </div>);
 };
 
